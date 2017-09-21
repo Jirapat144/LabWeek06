d5cf14dffde6ab6053c69bd9b999621357265fd9
Jirapat17412 <tangmo17412@hotmail.com>
2017-09-21 19:13:30 +0700

    ED by EX2

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -1,18 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace W06Assignment
 {
-    static void Main()
+    class Program
     {
-        int a = 2;
-        float f = 3.5f;
-        double d = 4.99d;
-        decimal money = 1254841354;
-        Console.WriteLine(a + " " + f + " " + d + " " + money);
+        static void Main()
+        {
+            int B, L;
+            Console.WriteLine("Enter the Begining and the Lasting Number for finding the primed numbers of 7 : ");
+            B = int.Parse(Console.ReadLine());
+            L = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", (PrimeSeven(B, L)));
+            Console.ReadKey();
+        }
+
+        static int PrimeSeven(int b, int l)
+        {
+            int S = 1;
+            for (int i = b; i <= l; i++)
+            {
+                if (i % 7 == 0) S = i;
+            }
+            return S;
+
+        }
+
     }
 }
-
-
-
-
-
